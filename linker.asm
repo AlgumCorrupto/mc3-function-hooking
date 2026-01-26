c1be6f8ee624eb9a638063fe68ca620bb160735c
Paulo Artur <pauloarturvillaca@gmail.com>
2026-01-25 22:17:59 -0300

    makefile is a bitch

--- linker.asm
+++ linker.asm
@@ -14,17 +14,20 @@
 .definelabel translation_manager_ptr, 0x6BE860
 .definelabel dword_61B1E0, 0x61B1E0
 
+c_code_addr equ 0x61C860 ; where the C code will be stored in the executable
+hello_world_offs equ 0x00000018 ; the offset of the hello_world function relative to the start of the c_code_addr
+                                ; you can get it by running mips64r5900el-ps2-elf-nm -n hello.c
+hello_world_addr equ c_code_addr + hello_world_offs ; absolute hello world function address
+
 ; writing the compiled C code inside unused area of the executable
-.org 0x61C860
+.org c_code_addr
     .importobj "./hello.o"
 
-; patching starting from 0x005A0F74 to call the hello_world function defined in ./src/mc3hello.c
+; patching starting from 0x005A0F74 to call the hello_world function defined in ./src/hello.c
 .org 0x005A0F74
     addiu sp, sp, -4
     sw    ra, 0(sp)
-    jal 0x61C8A8 ; where the .o file was imported + the offset of hello_world function (that you can get with
-                 ; mips64r5900el-ps2-elf-nm -S hello.o
-                 ; (0x61C860 + 0x48) 
+    jal hello_world_addr
     nop
     lw ra, 0(sp)
     addiu sp, sp, 0x4
